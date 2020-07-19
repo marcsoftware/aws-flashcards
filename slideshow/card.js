6c4001b37ba1791d0038ccf7e7fd19803013f148
marc <marcdavidmelcher@gmail.com>
2020-07-18 17:30:36 -0700

    made flashcard mode

---
+++ slideshow/card.js
@@ -0,0 +1,57 @@
+//file:///C:/Users/marc/Desktop/guide/notes/microservices.note
+window.onload=parse;
+
+
+
+// requires folder name
+var output;
+var slides;
+var total;
+function parse(){
+     
+    document.getElementById("output").addEventListener("mousedown", nextSlide); 
+    slides=text.split('<br>');
+    
+    total=slides.length-1;
+    draw();
+   
+}
+
+function parseHeaders(){
+   // output += slides[count].replace(/(\d+\.\ .*\n)/g,'<h1>$1</h1>');    
+
+}
+
+
+
+var count=0;
+function draw(){
+
+   output = slides[count].replace(/(.*\.jpg|.*\.gif)/g,`<p class="label">$1<img src="${folder_name}/$1" /></p>`);
+
+   output=output.split('|')[0];
+   output=output.replace(/[\r\n]/g,'');
+   parseHeaders();
+   var words = slides[count].replace(/(.*\.jpg|.*\.gif)/g,``);
+   document.getElementById('output').innerHTML=output;
+   document.getElementById('words').innerHTML=words;
+
+}
+
+function nextSlide(){
+    count++;
+    if(count==total){
+        alert('you won');
+    }
+    draw();
+    document.title=count+'/'+total;
+}
+
+function prevSlide(){
+    count--;
+    if(count==total){
+        alert('you won');
+    }
+    draw();
+    document.title=count+'/'+total;
+}
---
+++ slideshow/card.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<meta charset="UTF-8">
+
+<link rel="stylesheet" type="text/css" href="card.css">
+<html>
+
+
+
+
+
+<script>
+    
+
+var text = `<?php 
+
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "slideshow";
+
+                $video =$_GET['video'];
+
+                // Create connection
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                //$sql = "SELECT imageID,task FROM $dbname where video='$video' and task!='' ";
+                $sql = "SELECT imageID,task FROM $dbname where video='$video'  ";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo "" . $row["imageID"]. " |" . $row["task"].  "<br>";
+                    }
+                } else {
+                    echo "0 results";
+                }
+                $conn->close();
+
+
+
+ ?>`;
+
+ 
+</script>
+
+
+<script type="text/javascript">
+
+
+
+    var folder_name='aws';
+
+
+</script>
+<script type="text/javascript" src="card.js"></script>
+<body>
+
+
+<table>
+  <tr>
+    <th id='output'></th>
+  
+  </tr>
+   <tr>
+    <th id='words'>Company</th>
+  
+  </tr>
+
+</table>
+
+<input type='button' onclick='prevSlide()' value='<<<<<<<<<<<<<<<<<<'/>
+</body>
+</html> 
